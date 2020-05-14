e7819d9b61b72bd2c025776ec39f476f6d6274bb
Yoga Hanggara <yohang88@gmail.com>
2020-05-14 23:05:21 +0700

    minor revision bukti pendaftaran
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- resources/views/pdf/applicant.blade.php
+++ resources/views/pdf/applicant.blade.php
@@ -28,14 +28,18 @@
   <tr>
     <td align="center" style="padding: 30px">
       <p><img src="data:image/png;base64, {{ base64_encode(QrCode::errorCorrection('H')->format('png')->merge('/storage/pikobar.png')->margin(0)->size(320)->generate($applicant->registration_code)) }}"></p>
+      <h1 style="font-size: 14pt; margin: 0">Nomor Pendaftaran:</h1>
       <h1 style="font-size: 48pt; margin: 0">{{ $applicant->registration_code }}</h1>
+
+      <h1 style="font-size: 14pt; margin: 0; margin-top: 15px">Nama Peserta:</h1>
+      <h1 style="font-size: 14pt; margin: 0; font-weight: normal">{{ $applicant->name }}</h1>
     </td>
   </tr>
   <tr>
     <td style="padding: 30px">
-      <h3 style="margin: 5px 0; font-size: 14px">Keterangan</h3>
+      <h3 style="margin: 5px 0; font-size: 14px">Bukti Pendaftaran Ini Bukan Undangan</h3>
       <p style="margin: 0; font-size: 14px">Cek status pendaftaran Anda pada website <strong>https://rapidtest.pikobar.jabarprov.go.id</strong> <br /><br />
-        Hanya yang sudah diverifikasi oleh Dinas Kesehatan Provinsi atau Dinas Kesehatan Kabupaten/Kota akan mendapatkan undangan untuk mengikuti Rapid Test.</p>
+        Hanya yang sudah diverifikasi oleh Dinas Kesehatan Provinsi atau Dinas Kesehatan Kabupaten/Kota akan mendapatkan undangan untuk mengikuti COVID-19 Test. Undangan akan dikirimkan melalui SMS, Whatsapp, dan Email yang digunakan untuk mendaftar.</p>
     </td>
   </tr>
   </tbody>
