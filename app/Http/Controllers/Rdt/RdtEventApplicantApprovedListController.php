fbd22cd7efd9d6caf4ad7e3dee48feb4d4ecd77d
Yoga Hanggara <yohang88@gmail.com>
2020-06-26 11:47:54 +0700

    sorting applicants
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Controllers/Rdt/RdtEventApplicantApprovedListController.php
+++ app/Http/Controllers/Rdt/RdtEventApplicantApprovedListController.php
@@ -18,14 +18,21 @@ class RdtEventApplicantApprovedListController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $perPage = $request->input('per_page', 15);
+        $perPage    = $request->input('per_page', 15);
+        $sortBy     = $request->input('sort_by', 'name');
+        $sortOrder  = $request->input('sort_order', 'asc');
 
         if ($perPage > 100) {
             $perPage = 15;
         }
 
+        if (in_array($sortBy, ['name', 'created_at']) === false) {
+            $sortBy = 'name';
+        }
+
         $records = RdtApplicant::query();
         $records->whereEnum('status', RdtApplicantStatus::APPROVED());
+        $records->orderBy($sortBy, $sortOrder);
 
         return RdtApplicantResource::collection($records->paginate($perPage));
     }
