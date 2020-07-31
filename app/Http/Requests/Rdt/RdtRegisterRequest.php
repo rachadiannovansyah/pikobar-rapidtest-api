362f331dceb1a0d5638ffb5ccb1c1511c5b6c047
Oky saputra <oky31@nullreborn.com>
2020-08-01 01:53:55 +0700

    add min phone number be 11 digit

--- app/Http/Requests/Rdt/RdtRegisterRequest.php
+++ app/Http/Requests/Rdt/RdtRegisterRequest.php
@@ -38,7 +38,7 @@ class RdtRegisterRequest extends FormRequest
             'district_code'        => ['required', 'exists:areas,code_kemendagri'],
             'village_code'         => ['required', 'exists:areas,code_kemendagri'],
             'email'                => ['required', 'email'],
-            'phone_number'         => 'required',
+            'phone_number'         => ['required', 'min:11','max:13'],
             'gender'               => ['required', new EnumValueRule(Gender::class)],
             'person_status'        => ['required', new EnumValueRule(PersonCaseStatusEnum::class)],
             'birth_date'           => ['required', 'date'],
