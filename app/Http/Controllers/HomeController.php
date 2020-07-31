d5f628b094873549e1fb3cf9bc29efe501097d62
Yoga Hanggara <yohang88@gmail.com>
2020-07-31 22:59:40 +0700

    db health check
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,9 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // try to connect DB health check
+        $checkDbConnection = DB::connection()->getPdo();
+
         return [
             'app'      => config('app.name'),
             'hostname' => gethostname(),
