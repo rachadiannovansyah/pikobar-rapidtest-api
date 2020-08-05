862d92383b354215ee5e5efd3cf749ddf1cc737e
Yoga Hanggara <yohang88@gmail.com>
2020-08-06 05:39:53 +0700

    remove email mandatory on register
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Requests/Rdt/RdtRegisterRequest.php
+++ app/Http/Requests/Rdt/RdtRegisterRequest.php
@@ -37,7 +37,7 @@ class RdtRegisterRequest extends FormRequest
             'city_code'            => ['required', 'exists:areas,code_kemendagri'],
             'district_code'        => ['required', 'exists:areas,code_kemendagri'],
             'village_code'         => ['required', 'exists:areas,code_kemendagri'],
-            'email'                => ['required', 'email'],
+            'email'                => ['email'],
             'phone_number'         => ['required', 'min:11','max:13'],
             'gender'               => ['required', new EnumValueRule(Gender::class)],
             'person_status'        => ['required', new EnumValueRule(PersonCaseStatusEnum::class)],
