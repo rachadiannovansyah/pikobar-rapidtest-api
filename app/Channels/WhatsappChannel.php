71dcc4e067bc3761fd2c37faedcad34dfd0ef57b
Yoga Hanggara <yohang88@gmail.com>
2020-08-20 16:01:43 +0700

    catch all notification sms/wa to developer instead of real number (staging/development)
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Channels/WhatsappChannel.php
+++ app/Channels/WhatsappChannel.php
@@ -4,27 +4,52 @@ namespace App\Channels;
 
 use AsyncAws\Sqs\Input\SendMessageRequest;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class WhatsappChannel
 {
     /**
      * Send the given notification.
      *
-     * @param  mixed  $notifiable
-     * @param  \Illuminate\Notifications\Notification $notification
-     * @return void
+     * @param mixed $notifiable
+     * @param \Illuminate\Notifications\Notification $notification
+     * @return bool
+     * @throws \Exception
      */
     public function send($notifiable, Notification $notification)
     {
         $message = $notification->toWhatsapp($notifiable);
 
-        $aws = app('aws');
+        $phoneNumber = $this->getPhoneNumber($notifiable);
 
-        $sqs = $aws->sqs();
+        if ($phoneNumber instanceof Collection) {
+            $phoneNumber->each(function ($phoneNumberValue) use ($message) {
+                $this->pushQueue($phoneNumberValue, $message);
+            });
 
+            return true;
+        }
+
+        $this->pushQueue($phoneNumber, $message);
+
+        return true;
+    }
+
+    protected function cleanPhoneNumber($phoneNumber)
+    {
+        return preg_replace('/^0{1}/', '62', $phoneNumber);
+    }
+
+    protected function pushQueue($phoneNumber, $message)
+    {
+        /**
+         * @var \AsyncAws\Core\AwsClientFactory $aws
+         * @var \AsyncAws\Sqs\SqsClient $sqs
+         */
+        $aws      = app('aws');
+        $sqs      = $aws->sqs();
         $queueUrl = $sqs->getQueueUrl(['QueueName' => 'wablast-queue'])->getQueueUrl();
-
-        $phoneNumber = preg_replace('/^0{1}/', '62', $notifiable->phone_number);
 
         $messageRequest = new SendMessageRequest([
             'QueueUrl'          => $queueUrl,
@@ -38,6 +63,32 @@ class WhatsappChannel
             'MessageBody' => $message,
         ]);
 
-        $sqs->sendMessage($messageRequest);
+        return $sqs->sendMessage($messageRequest);
+    }
+
+    /**
+     * In development/staging environment, we need to carefully send notifications.
+     * Redirect/send to developer instead to real numbers
+     * @param $notifiable
+     * @return string|\Illuminate\Support\Collection
+     * @throws \Exception
+     */
+    protected function getPhoneNumber($notifiable)
+    {
+        $currentEnvironment = config('app.env');
+
+        if ($currentEnvironment === 'production') {
+            return $this->cleanPhoneNumber($notifiable->phone_number);
+        }
+
+        $configPhoneNumbers = config('notifications.notify_to');
+
+        if (empty($configPhoneNumbers)) {
+            throw new \Exception("Please update config 'notifications.notify_to'");
+        }
+
+        return Str::of($configPhoneNumbers)->explode(',')->map(function ($phoneNumber) {
+            return $this->cleanPhoneNumber($phoneNumber);
+        });
     }
 }
