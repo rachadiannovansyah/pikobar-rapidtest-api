faa2c27e8d24ebb1d1b47c8151a3090ce56615ca
Yoga Hanggara <yohang88@gmail.com>
2020-08-20 18:06:20 +0700

    add enum revision 5 covid, confirmed, suspect, probable, close contact
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Enums/PersonCaseStatusEnum.php
+++ app/Enums/PersonCaseStatusEnum.php
@@ -5,6 +5,10 @@ namespace App\Enums;
 use Spatie\Enum\Enum;
 
 /**
+ * @method static self CONFIRMED()
+ * @method static self SUSPECT()
+ * @method static self PROBABLE()
+ * @method static self CLOSE_CONTACT()
  * @method static self ODP()
  * @method static self PDP()
  * @method static self OTG()
