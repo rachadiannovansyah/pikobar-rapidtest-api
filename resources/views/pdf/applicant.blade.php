78d19cd062162bb9ddd94452f6af4cb6ca305bbb
Yoga Hanggara <yohang88@gmail.com>
2020-08-23 00:36:51 +0700

    wording bukti pendaftaran
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- resources/views/pdf/applicant.blade.php
+++ resources/views/pdf/applicant.blade.php
@@ -13,6 +13,12 @@
     border: 1px solid black;
     padding: 5px;
   }
+
+  table.detail, table.detail th, table.detail td {
+    border: 0;
+    padding: 10px;
+    font-size: 12pt;
+  }
 </style>
 
 <table>
@@ -35,10 +41,62 @@
     </td>
   </tr>
   <tr>
+    <td>
+      <table class="detail">
+        @if ($applicant->pikobar_session_id)
+          <tr>
+            <th width="250px" align="right">Session ID</th>
+            <td>{{ $applicant->pikobar_session_id }}</td>
+          </tr>
+        @endif
+        <tr>
+          <th width="250px" align="right">Nomor Pendaftaran</th>
+          <td>{{ $applicant->registration_code }}</td>
+        </tr>
+        <tr>
+          <th width="250px" align="right">Tanggal Pendaftaran</th>
+          <td>{{ $applicant->created_at->setTimezone('Asia/Jakarta') }}</td>
+        </tr>
+        <tr>
+          <th width="250px" align="right">Nama Peserta</th>
+          <td>{{ $applicant->name }}</td>
+        </tr>
+        <tr>
+          <th width="250px" align="right">Kab/Kota Domisili</th>
+          <td>{{ optional($applicant->city)->name }}</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
     <td style="padding: 30px">
-      <h3 style="margin: 5px 0; font-size: 14px">Bukti Pendaftaran Ini Bukan Undangan</h3>
-      <p style="margin: 0; font-size: 14px">Cek status pendaftaran Anda pada website <strong>https://tesmasif.pikobar.jabarprov.go.id</strong> <br /><br />
-        Hanya yang sudah diverifikasi oleh Dinas Kesehatan Provinsi atau Dinas Kesehatan Kabupaten/Kota akan mendapatkan undangan untuk mengikuti tes masif COVID-19. Undangan akan dikirimkan melalui SMS, Whatsapp, dan Email yang digunakan untuk mendaftar.</p>
+      <h2 style="margin: 5px 0; font-size: 16px">Info Penting</h2>
+
+      @if ($applicant->status->isEqual(\App\Enums\RdtApplicantStatus::NEW()))
+      <div style="background-color: #ffd4d4; padding: 10px 20px; margin-top: 20px; font-size: 11pt">
+        <h3>Menunggu Verifikasi</h3>
+        <p>Hanya yang sudah diverifikasi oleh Dinas Kesehatan Provinsi atau Dinas Kesehatan Kabupaten/Kota akan mendapatkan undangan untuk mengikuti tes masif COVID-19. Undangan akan dikirimkan melalui SMS, Whatsapp, dan Email yang digunakan untuk mendaftar.</p>
+      </div>
+      @endif
+
+      <ol style="font-size: 11pt">
+        <li><strong>Simpan Nomor Pendaftaran Anda.</strong><br/>
+        Nomor Pendaftaran digunakan untuk mendapatkan undangan dan hasil test.</li>
+
+        <li style="margin-top: 14px"><strong>Cek status pendaftaran/undangan Anda melalui website.</strong><br/>
+          Buka <a href="https://tesmasif.pikobar.jabarprov.go.id">https://tesmasif.pikobar.jabarprov.go.id</a> dan masukkan Nomor Pendaftaran.</li>
+
+        <li style="margin-top: 14px"><strong>Jangan membagikan nomor pendaftaran ini kepada orang lain.</strong><br/>
+          Pastikan identitas Anda tidak digunakan orang lain.</li>
+
+        <li style="margin-top: 14px"><strong>Tes Masif COVID-19 ini tidak dipungut biaya.</strong></li>
+
+        <li style="margin-top: 14px"><strong>Unduh aplikasi PIKOBAR (Android/iOS).</strong><br/>
+          Untuk mengakses informasi perkembangan terkini penanganan COVID-19 di Jawa Barat, unduh di <a href="https://bit.ly/PIKOBAR-V1">https://bit.ly/PIKOBAR-V1</a></li>
+
+        <li style="margin-top: 14px"><strong>Informasi dan pertanyaan lebih lanjut hubungi Pusat Bantuan PIKOBAR.</strong><br/>
+          Hotline <a href="https://api.whatsapp.com/send?phone=628112093306">08112093306</a> atau Gugus Tugas/Dinas Kesehatan Kota/Kabupaten/Provinsi setempat.</li>
+      </ol>
     </td>
   </tr>
   </tbody>
