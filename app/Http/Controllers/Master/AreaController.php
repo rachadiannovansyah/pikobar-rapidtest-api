64c5ef70a28cf5e0c140791d784262874c56e0e0
Yoga Hanggara <yohang88@gmail.com>
2020-08-27 11:20:23 +0700

    refactor area controller
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Controllers/Master/AreaController.php
+++ app/Http/Controllers/Master/AreaController.php
@@ -6,6 +6,7 @@ use App\Entities\Area;
 use App\Http\Controllers\ApiController;
 use App\Http\Resources\AreaResource as AreaResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class AreaController extends ApiController
 {
@@ -13,11 +14,12 @@ class AreaController extends ApiController
      * Display a listing of the resource.
      *
      * @param Request $request
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @return AnonymousResourceCollection
      */
     public function index(Request $request)
     {
         $records = Area::query();
+        $records->orderBy('name');
 
         if ($request->has('depth')) {
             $records = $records->where('depth', $request->input('depth'));
@@ -25,19 +27,26 @@ class AreaController extends ApiController
 
         if ($request->has('parent_code_kemendagri')) {
             $records = $records->where('parent_code_kemendagri', $request->input('parent_code_kemendagri'));
-        } else {
-            $records = $records->where('parent_code_kemendagri', '32');
+
+            return $this->responseResourceCollection($records);
         }
 
-        $records->orderBy('name');
+        $records = $records->where('parent_code_kemendagri', '32');
 
+        return $this->responseResourceCollection($records);
+    }
+
+    protected function responseResourceCollection($records)
+    {
         return AreaResource::collection($records->get());
     }
 
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Area $area
+     * @return AreaResource|\Illuminate\Http\Response
      */
     public function show(Request $request, Area $area)
     {
