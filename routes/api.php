1a386345426e80beb9373a80e67db80e54c40193
Yoga Hanggara <yohang88@gmail.com>
2020-08-28 18:34:49 +0700

    routes code sniffer
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,11 @@ use Illuminate\Support\Facades\URL;
 $proxyUrl    = config('proxy.url');
 $proxyScheme = config('proxy.scheme');
 
-if(!empty($proxyUrl)) {
+if (!empty($proxyUrl)) {
     URL::forceRootUrl($proxyUrl);
 }
 
-if(!empty($proxyScheme)) {
+if (!empty($proxyScheme)) {
     URL::forceScheme($proxyScheme);
 }
 
@@ -47,42 +47,47 @@ Route::group(['middleware' => 'auth:api'], function () {
 });
 
 // RDT Events
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::get('rdt/events','Rdt\RdtEventController@index')->middleware('can:viewAny,'. RdtEvent::class);
-    Route::get('rdt/events/{rdtEvent}','Rdt\RdtEventController@show')->middleware('can:view,rdtEvent');
-    Route::post('rdt/events','Rdt\RdtEventController@store')->middleware('can:create,'.RdtEvent::class);
-    Route::put('rdt/events/{rdtEvent}','Rdt\RdtEventController@update')->middleware('can:update,rdtEvent');
-    Route::delete('rdt/events/{rdtEvent}','Rdt\RdtEventController@destroy')->middleware('can:delete,rdtEvent');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('rdt/events', 'Rdt\RdtEventController@index')->middleware('can:viewAny,' . RdtEvent::class);
+    Route::get('rdt/events/{rdtEvent}', 'Rdt\RdtEventController@show')->middleware('can:view,rdtEvent');
+    Route::post('rdt/events', 'Rdt\RdtEventController@store')->middleware('can:create,' . RdtEvent::class);
+    Route::put('rdt/events/{rdtEvent}', 'Rdt\RdtEventController@update')->middleware('can:update,rdtEvent');
+    Route::delete('rdt/events/{rdtEvent}', 'Rdt\RdtEventController@destroy')->middleware('can:delete,rdtEvent');
 });
 
 // RDT Event Invitations Participants
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::get('rdt/events/{rdtEvent}/participants','Rdt\RdtEventParticipantListController')->middleware('can:view,rdtEvent');
-    Route::post('rdt/events/{rdtEvent}/participants','Rdt\RdtEventParticipantAddController');
-    Route::post('rdt/events/{rdtEvent}/participants-remove','Rdt\RdtEventParticipantRemoveController');
-    Route::post('rdt/events/{rdtEvent}/participants-notify','Rdt\RdtEventNotifyParticipantController');
-    Route::post('rdt/events/{rdtEvent}/participants-notify-result','Rdt\RdtEventNotifyTestResultController');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('rdt/events/{rdtEvent}/participants', 'Rdt\RdtEventParticipantListController')
+        ->middleware('can:view,rdtEvent');
+
+    Route::post('rdt/events/{rdtEvent}/participants', 'Rdt\RdtEventParticipantAddController');
+    Route::post('rdt/events/{rdtEvent}/participants-remove', 'Rdt\RdtEventParticipantRemoveController');
+    Route::post('rdt/events/{rdtEvent}/participants-notify', 'Rdt\RdtEventNotifyParticipantController');
+    Route::post('rdt/events/{rdtEvent}/participants-notify-result', 'Rdt\RdtEventNotifyTestResultController');
 
     Route::post('rdt/events/{rdtEvent}/participants-import', 'Rdt\RdtEventParticipantImportController');
-    Route::get('rdt/events/{rdtEvent}/participants-export','Rdt\RdtEventParticipantListExportController')->middleware('can:view,rdtEvent');
+    Route::get('rdt/events/{rdtEvent}/participants-export', 'Rdt\RdtEventParticipantListExportController')
+        ->middleware('can:view,rdtEvent');
 
-    Route::post('rdt/events/{rdtEvent}/participants-import-results','Rdt\RdtEventParticipantImportResultController');
+    Route::post('rdt/events/{rdtEvent}/participants-import-results', 'Rdt\RdtEventParticipantImportResultController');
 
     Route::put('rdt/events/{rdtEvent}/participants-set-labcode', 'Rdt\RdtEventParticipantSetLabCodeController');
 });
 
 // RDT Applicants
-Route::group(['middleware' => 'auth:api'], function() {
-    Route::get('rdt/applicants','Rdt\RdtApplicantController@index')->middleware('can:viewAny,'.RdtApplicant::class);
-    Route::get('rdt/applicants/{rdtApplicant}','Rdt\RdtApplicantController@show')->middleware('can:view,rdtApplicant');
-    Route::post('rdt/applicants','Rdt\RdtApplicantController@store')->middleware('can:create,'.RdtApplicant::class);
-    Route::put('rdt/applicants/{rdtApplicant}', 'Rdt\RdtApplicantController@update')->middleware('can:update,rdtApplicant');
-    Route::delete('rdt/applicants/{rdtApplicant}','Rdt\RdtApplicantController@destroy')->middleware('can:delete,rdtApplicant');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('rdt/applicants', 'Rdt\RdtApplicantController@index')->middleware('can:viewAny,' . RdtApplicant::class);
+    Route::get('rdt/applicants/{rdtApplicant}', 'Rdt\RdtApplicantController@show')->middleware('can:view,rdtApplicant');
+    Route::post('rdt/applicants', 'Rdt\RdtApplicantController@store')->middleware('can:create,' . RdtApplicant::class);
+    Route::put('rdt/applicants/{rdtApplicant}', 'Rdt\RdtApplicantController@update')
+        ->middleware('can:update,rdtApplicant');
+    Route::delete('rdt/applicants/{rdtApplicant}', 'Rdt\RdtApplicantController@destroy')
+        ->middleware('can:delete,rdtApplicant');
 });
 
 Route::group(['middleware' => 'guest:api'], function () {
     // API for Master data
-    Route::prefix('master')->namespace('Master')->group(function() {
+    Route::prefix('master')->namespace('Master')->group(function () {
         Route::get('areas', 'AreaController@index');
         Route::get('areas/{area}', 'AreaController@show');
     });
