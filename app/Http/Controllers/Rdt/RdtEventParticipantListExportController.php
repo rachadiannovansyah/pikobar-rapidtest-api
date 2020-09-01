5459656e41c46574a868cd62869f7c385a243632
Yoga Hanggara <yohang88@gmail.com>
2020-09-01 20:27:51 +0700

    temp fix content disposition
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Controllers/Rdt/RdtEventParticipantListExportController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantListExportController.php
@@ -46,7 +46,7 @@ class RdtEventParticipantListExportController extends Controller
         }
 
         $fileName           = $this->getFileName($rdtEvent);
-        $contentDisposition = $response->header('Content-Disposition');
+        $contentDisposition = "attachment; filename={$fileName};";
 
         return response($response->body(), 200, [
             'Content-Length'      => $response->header('Content-Length'),
