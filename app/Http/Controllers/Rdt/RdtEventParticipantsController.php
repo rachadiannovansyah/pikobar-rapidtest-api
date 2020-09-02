24476031b28534e1c44e3a3cb1e08db39b8de863
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-02 12:00:21 +0700

    perbaikan menggunakan event_code sebagai parameter

--- app/Http/Controllers/Rdt/RdtEventParticipantsController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantsController.php
@@ -7,16 +7,15 @@ use App\Entities\RdtInvitation;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Log;
 
 class RdtEventParticipantsController extends Controller
 {
-    public function __invoke($id, Request $request)
+    public function __invoke($eventCode, Request $request)
     {
         $per_page  = $request->input('per_page', 50);
         $keyword   = $request->input('keyword');
 
-        $event = RdtEvent::findOrFail($id);
+        $event = RdtEvent::where('event_code', $eventCode)->firstOrFail();
         $invitations = RdtInvitation::leftJoin('rdt_applicants', 'rdt_applicants.id', '=', 'rdt_invitations.rdt_applicant_id')
         ->select('rdt_applicants.name', 'rdt_applicants.registration_code', 'rdt_invitations.lab_code_sample', 'rdt_applicants.created_at', 'rdt_applicants.attended_at')
         ->where('rdt_invitations.rdt_event_id', $event->id);
