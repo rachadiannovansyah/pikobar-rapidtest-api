2a6513597b89f187731146423f6dcf4125d42531
Yoga Hanggara <yohang88@gmail.com>
2020-09-03 08:33:34 +0700

    cleanup some codes
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Controllers/Rdt/RdtEventParticipantListExportController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantListExportController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class RdtEventParticipantListExportController extends Controller
@@ -20,10 +21,14 @@ class RdtEventParticipantListExportController extends Controller
      */
     public function __invoke(Request $request, RdtEvent $rdtEvent)
     {
-        // header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: *');
         header('Access-Control-Allow-Headers: *');
         header('Access-Control-Expose-Headers: *');
+
+        Log::info('EVENT_PARTICIPANT_LIST_EXPORT', [
+            'event_id' => $rdtEvent->id,
+            'user_id'  => $request->user()->id,
+        ]);
 
         try {
             $serviceUrl = config('services.internal.participant_export');
@@ -32,25 +37,56 @@ class RdtEventParticipantListExportController extends Controller
                 'rdt_event_id' => $rdtEvent->id,
             ]);
         } catch (ConnectionException $exception) {
-            return response()->json([
-                'error'   => "CONNECTION_FAILED",
-                'message' => "Cannot connect to service. Message: {$exception->getMessage()}",
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->responseConnectFailed($rdtEvent, $request, $exception);
         }
 
         if ($response->failed()) {
-            return response()->json([
-                'error'   => "FETCH_FAILED",
-                'message' => "Error fetch data from service.",
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->responseFetchFailed($rdtEvent, $request);
         }
+
+        return $this->responseSuccess($rdtEvent, $request, $response);
+    }
+
+    protected function responseConnectFailed(RdtEvent $rdtEvent, Request $request, ConnectionException $exception)
+    {
+        Log::info('EVENT_PARTICIPANT_LIST_EXPORT_CONNECT_FAILED', [
+            'event_id' => $rdtEvent->id,
+            'user_id'  => $request->user()->id,
+            'message'  => $exception->getMessage(),
+        ]);
+
+        return response()->json([
+            'error'   => "CONNECTION_FAILED",
+            'message' => "Cannot connect to service. Message: {$exception->getMessage()}",
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    protected function responseFetchFailed(RdtEvent $rdtEvent, Request $request)
+    {
+        Log::info('EVENT_PARTICIPANT_LIST_EXPORT_FETCH_FAILED', [
+            'event_id' => $rdtEvent->id,
+            'user_id'  => $request->user()->id,
+        ]);
+
+        return response()->json([
+            'error'   => "FETCH_FAILED",
+            'message' => "Error fetch data from service.",
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    protected function responseSuccess(RdtEvent $rdtEvent, Request $request, $serviceResponse)
+    {
+        Log::info('EVENT_PARTICIPANT_LIST_EXPORT_SUCCESS', [
+            'event_id' => $rdtEvent->id,
+            'user_id'  => $request->user()->id,
+        ]);
 
         $fileName           = $this->getFileName($rdtEvent);
         $contentDisposition = "attachment; filename=\"{$fileName}\";";
 
-        return response($response->body(), 200, [
-            'Content-Length'      => $response->header('Content-Length'),
-            'Content-Type'        => $response->header('Content-Type'),
+        return response($serviceResponse->body(), Response::HTTP_OK, [
+            'Content-Length'      => $serviceResponse->header('Content-Length'),
+            'Content-Type'        => $serviceResponse->header('Content-Type'),
             'Content-Disposition' => $contentDisposition,
         ]);
     }
