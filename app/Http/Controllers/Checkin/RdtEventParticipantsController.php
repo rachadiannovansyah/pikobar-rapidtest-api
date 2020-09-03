801373d180e8573b5121b531acccd7acc72f5ff3
Yoga Hanggara <yohang88@gmail.com>
2020-09-03 09:00:29 +0700

    cleanup some codes use PSR12
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Controllers/Checkin/RdtEventParticipantsController.php
+++ app/Http/Controllers/Checkin/RdtEventParticipantsController.php
@@ -18,14 +18,25 @@ class RdtEventParticipantsController extends Controller
         $keyword   = $request->input('keyword');
 
         $event = RdtEvent::where('event_code', $eventCode)->firstOrFail();
-        $invitations = RdtInvitation::leftJoin('rdt_applicants', 'rdt_applicants.id', '=', 'rdt_invitations.rdt_applicant_id')
-        ->select('rdt_applicants.name', 'rdt_applicants.registration_code', 'rdt_invitations.lab_code_sample', 'rdt_applicants.created_at', 'rdt_applicants.attended_at')
+        $invitations = RdtInvitation::leftJoin(
+            'rdt_applicants',
+            'rdt_applicants.id',
+            '=',
+            'rdt_invitations.rdt_applicant_id'
+        )
+        ->select(
+            'rdt_applicants.name',
+            'rdt_applicants.registration_code',
+            'rdt_invitations.lab_code_sample',
+            'rdt_applicants.created_at',
+            'rdt_applicants.attended_at'
+        )
         ->where('rdt_invitations.rdt_event_id', $event->id);
 
         if ($keyword) {
             $invitations->where('rdt_applicants.name', 'like', "%$keyword%");
         }
-        
+
         return RdtApplicantInvitationResource::collection($invitations->paginate($perPage));
     }
 }
--- app/Notifications/TestResult.php
+++ app/Notifications/TestResult.php
@@ -5,7 +5,6 @@ namespace App\Notifications;
 use App\Channels\SmsChannel;
 use App\Channels\WhatsappChannel;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
 class TestResult extends Notification
@@ -25,7 +24,7 @@ class TestResult extends Notification
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -35,20 +34,21 @@ class TestResult extends Notification
 
     public function toSms($notifiable)
     {
-        $message  = 'Sampurasun, hasil Tes COVID Anda sudah keluar. ';
+        $message = 'Sampurasun, hasil Tes COVID Anda sudah keluar. ';
         $message .= 'Silahkan buka tautan bit.ly/tesmasif dan input Nomor Pendaftaran ';
-        $message .=  $notifiable->registration_code . ' untuk melihat hasil tes.';
+        $message .= $notifiable->registration_code . ' untuk melihat hasil tes.';
 
         return $message;
     }
 
     public function toWhatsapp($notifiable)
     {
-        $message   = '*Yth. ' . $notifiable->name . '* ';
-        $message  .= 'Sampurasun, hasil Tes COVID-19 Anda sudah keluar. ';
-        $message  .= 'Untuk mengetahui hasilnya, silahkan buka tautan https://bit.ly/tesmasif dan ';
-        $message  .= 'masukkan Nomor Pendaftaran berikut: *' . $notifiable->registration_code . '* untuk mendapatkan hasil tes. ';
-        $message  .= 'Hatur nuhun.';
+        $message = '*Yth. ' . $notifiable->name . '* ';
+        $message .= 'Sampurasun, hasil Tes COVID-19 Anda sudah keluar. ';
+        $message .= 'Untuk mengetahui hasilnya, silahkan buka tautan https://bit.ly/tesmasif dan ';
+        $message .= "masukkan Nomor Pendaftaran berikut:
+            *{$notifiable->registration_code}* untuk mendapatkan hasil tes. ";
+        $message .= 'Hatur nuhun.';
 
         return $message;
     }
@@ -56,7 +56,7 @@ class TestResult extends Notification
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
