c10fe02c303b97d0a6fd6f789165a78e7825eff7
Oky saputra <oky31@nullreborn.com>
2020-09-03 11:23:04 +0700

    move notification using laravel notification

--- app/Http/Controllers/Rdt/RdtEventParticipantImportController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantImportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Rdt;
 
+use App\Channels\SmsChannel;
+use App\Channels\WhatsappChannel;
 use App\Entities\RdtApplicant;
 use App\Entities\RdtEvent;
 use App\Entities\RdtInvitation;
@@ -9,59 +11,38 @@ use App\Enums\RdtApplicantStatus;
 use App\Enums\RdtEventStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Rdt\RdtInvitationImportRequest;
-use AsyncAws\Core\AwsClientFactory;
-use AsyncAws\Sqs\Input\GetQueueUrlRequest;
-use AsyncAws\Sqs\Input\SendMessageRequest;
+use App\Notifications\RdtEventInvitation;
 use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 use Carbon\Carbon;
 
 class RdtEventParticipantImportController extends Controller
 {
-
-    const SMS_QUEUE_NAME = 'smsblast-queue';
-
-    const WA_QUEUE_NAME = 'wablast-queue';
-
     const NOTIFY_SMS = 'sms';
 
     const NOTIFY_WA = 'wa';
 
     const NOTIFY_BOTH = 'both';
 
-    private $sqs;
-
-    public function __construct()
-    {
-
-        // set credential sqs
-        $factory = new AwsClientFactory([
-            'region'            => config('aws.region'),
-            'accessKeyId'       => config('aws.key'),
-            'accessKeySecret'   => config('aws.secret')
-        ]);
-
-        $this->sqs = $factory->sqs();
-    }
-
     public function __invoke(RdtInvitationImportRequest $request, RdtEvent $rdtEvent)
     {
-        $reader = ReaderEntityFactory::createXLSXReader();
-
-        $reader->open($request->file->path());
 
         $count = 0;
-
         $rdtEvent->status = RdtEventStatus::PUBLISHED();
         $rdtEvent->save();
 
+        $reader = ReaderEntityFactory::createXLSXReader();
+        $reader->open($request->file->path());
+
         foreach ($reader->getSheetIterator() as $sheet) {
+
             foreach ($sheet->getRowIterator() as $key => $row) {
+
                 $rowArray = $row->toArray();
 
                 if ($key > 1) {
-                    $count++;
 
-                    $participant = [
+                    $count++;
+                    $fileImport = [
                         'registration_code'     => $rowArray[0],
                         'rdt_event_id'          => $rowArray[1],
                         'rdt_event_schedule_id' => $rowArray[2],
@@ -73,13 +54,11 @@ class RdtEventParticipantImportController extends Controller
                         'notify_method'         => $rowArray[8] // SMS/WA/BOTH
                     ];
 
+                    $applicant  = $this->fillApplicant($fileImport);
+                    $invitation = $this->fillInvitation($applicant, $fileImport);
 
-                    $applicant  = $this->fillApplicant($participant);
-
-                    $invitation = $this->fillInvitation($applicant, $participant);
-
-                    if (strtolower($participant['notify']) === 'yes') {
-                        $this->pushNotification($participant, $rdtEvent, $applicant);
+                    if (strtolower($fileImport['notify']) === 'yes') {
+                        $this->pushNotification($fileImport, $rdtEvent, $applicant);
                         $invitation->notified_at = Carbon::now();
                         $invitation->save();
                     }
@@ -124,107 +103,20 @@ class RdtEventParticipantImportController extends Controller
         return $rdtInvitation;
     }
 
-    private function getQueueUrl($queueName)
+    private function pushNotification($fileImport, $event, $applicant)
     {
-
-        return $this->sqs->getQueueUrl(new GetQueueUrlRequest([
-            'QueueName' => $queueName
-        ]))->getQueueUrl();
-    }
-
-    private function sendMessageToQueue($queueName, $phoneNumber, $message)
-    {
-
-        $messageRequest = new SendMessageRequest([
-            'QueueUrl'          => $this->getQueueUrl($queueName),
-            'DelaySeconds'      => 10,
-            'MessageAttributes' => [
-                'PhoneNumber'   => [
-                    'DataType'  => 'String',
-                    'StringValue'   => $phoneNumber
-                ]
-            ],
-            'MessageBody'       => $message
-        ]);
-
-
-        $this->sqs->sendMessage($messageRequest);
-    }
-
-    private function reformatPhoneNumber($phoneNumber, $format = 'sms')
-    {
-
-        if ($format === 'wa') {
-            return $this->reformatWa($phoneNumber);
+        if (strtolower($fileImport['notify_method']) === self::NOTIFY_WA) {
+            $applicant->notifyNow(new RdtEventInvitation($event), [WhatsappChannel::class]);
+            dd('wa');
         }
 
-        return $this->reformatSms($phoneNumber);
-    }
-
-    private function reformatSms($phoneNumber)
-    {
-        if ($phoneNumber[0] == '6') {
-            return substr_replace($phoneNumber, '0', 0, 2);
+        if (strtolower($fileImport['notify_method']) === self::NOTIFY_SMS) {
+            $applicant->notifyNow(new RdtEventInvitation($event), [SmsChannel::class]);
+            dd('sms');
         }
 
-        if ($phoneNumber[0] == '+') {
-            return substr_replace($phoneNumber, '0', 0, 3);
-        }
-
-        return $phoneNumber;
-    }
-
-    private function reformatWa($phoneNumber)
-    {
-        if ($phoneNumber[0] == '0') {
-            return substr_replace($phoneNumber, '62', 0, 1);
-        }
-
-        if ($phoneNumber[0] == '+') {
-            return substr_replace($phoneNumber, '', 0, 1);
-        }
-
-        return $phoneNumber;
-    }
-
-    private function messageWa($name, $hostName, $registrationCode)
-    {
-
-        $message  = 'Yth. ' . $name . ' Sampurasun, Anda diundang untuk melakukan Tes Masif COVID-19 oleh ' . $hostName;
-        $message .= ' Silakan buka tautan https://s.id/tesmasif2 dan masukkan Nomor Pendaftaran berikut: ';
-        $message .= $registrationCode . ' untuk melihat undangan. Hatur nuhun';
-
-        return $message;
-    }
-
-    private function messageSms($hostName, $registrationCode)
-    {
-
-        $message  = 'Sampurasun. Anda diundang Tes Masif COVID-19 ';
-        $message .= $hostName . '. Buka tautan s.id/tesmasif1 dan input nomor: ';
-        $message .= $registrationCode . ' untuk melihat undangan.';
-
-        return $message;
-    }
-
-    private function pushNotification($participant, $event, $applicant)
-    {
-        $phoneNumberWa  = $this->reformatPhoneNumber($participant['phone_number'], self::NOTIFY_WA);
-        $phoneNumberSms = $this->reformatPhoneNumber($participant['phone_number'], self::NOTIFY_SMS);
-        $messageWa      = $this->messageWa($applicant->name, $event->host_name, $applicant->registration_code);
-        $messageSms     = $this->messageSms($event->host_name, $applicant->registration_code);
-
-        if (strtolower($participant['notify_method']) === self::NOTIFY_WA) {
-            $this->sendMessageToQueue(self::WA_QUEUE_NAME, $phoneNumberWa, $messageWa);
-        }
-
-        if (strtolower($participant['notify_method']) === self::NOTIFY_SMS) {
-            $this->sendMessageToQueue(self::SMS_QUEUE_NAME, $phoneNumberSms, $messageSms);
-        }
-
-        if (strtolower($participant['notify_method']) === self::NOTIFY_BOTH) {
-            $this->sendMessageToQueue(self::WA_QUEUE_NAME, $phoneNumberWa, $messageWa);
-            $this->sendMessageToQueue(self::SMS_QUEUE_NAME, $phoneNumberSms, $messageSms);
+        if (strtolower($fileImport['notify_method']) === self::NOTIFY_BOTH) {
+            $applicant->notifyNow(new RdtEventInvitation($event), [WhatsappChannel::class, SmsChannel::class]);
         }
     }
 }
