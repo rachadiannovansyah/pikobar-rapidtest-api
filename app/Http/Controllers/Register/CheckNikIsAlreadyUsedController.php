e701f9c676b59eac7fbaf817fdeb041b01947d97
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-04 17:38:44 +0700

    remove \

--- app/Http/Controllers/Register/CheckNikIsAlreadyUsedController.php
+++ app/Http/Controllers/Register/CheckNikIsAlreadyUsedController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Register;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \Illuminate\Validation\ValidationException;
+use Validator;
 
 class CheckNikIsAlreadyUsedController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $validator = \Validator::make($request->all(), ['nik' => 'required|unique:rdt_applicants']);
+        $validator = Validator::make($request->all(), ['nik' => 'required|unique:rdt_applicants']);
         if ($validator->fails()) {
             throw  ValidationException::withMessages([
                 'nik' => 'Nik sudah pernah digunakan'
