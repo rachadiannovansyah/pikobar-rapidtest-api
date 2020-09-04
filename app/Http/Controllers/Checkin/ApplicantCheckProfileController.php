f0f8e8ddf65453f05cef54be9cc126b7669099be
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-04 11:24:33 +0700

    Update ApplicantCheckProfileController.php

--- app/Http/Controllers/Checkin/ApplicantCheckProfileController.php
+++ app/Http/Controllers/Checkin/ApplicantCheckProfileController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Checkin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Rdt\ApplicantsCekProfileRequest;
+use App\Http\Requests\Check\ApplicantCheckProfileRequest;
 use App\Entities\RdtApplicant;
 use App\Http\Resources\ApplicantProfileResource;
 
 class ApplicantCheckProfileController extends Controller
 {
-    public function __invoke(ApplicantsCekProfileRequest $request)
+    public function __invoke(ApplicantCheckProfileRequest $request)
     {
         $rdtApplicant = RdtApplicant::where('registration_code', $request->registration_code)->firstOrFail();
         return new ApplicantProfileResource($rdtApplicant);
