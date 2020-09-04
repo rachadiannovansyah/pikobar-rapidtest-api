39acc71567aa0effd4db5289d7001ec63e23e70b
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-04 13:46:33 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::post('rdt/survey', 'Rdt\RdtSurveyStoreController');
 Route::post('rdt/checkin', 'Rdt\RdtCheckinController');
 Route::post('rdt/event-check', 'Rdt\RdtEventCheckController');
 Route::post('checkin/event/participants', 'Checkin\RdtEventParticipantsController');
-Route::post('checkin/nik', 'Checkin\CheckNikIsAlreadyUsedController');
+Route::post('register/check-nik', 'Register\CheckNikIsAlreadyUsedController');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/user', 'Settings\ProfileController@index');
