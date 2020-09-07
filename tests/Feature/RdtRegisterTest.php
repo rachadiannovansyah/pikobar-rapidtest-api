0d766f34561dc266eb7fd984e2b7310b50c5f859
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-07 09:42:06 +0700

    change value have_interacted

--- tests/Feature/RdtRegisterTest.php
+++ tests/Feature/RdtRegisterTest.php
@@ -28,7 +28,7 @@ class RdtRegisterTest extends TestCase
             'symptoms_notes'       => 'Notes',
             'city_visited'         => 'jakarta',
             'congenital_disease'   => [1,2,3],
-            'have_interacted'      => 1,
+            'have_interacted'      => 'Tidak',
             'person_status'        => PersonCaseStatusEnum::CLOSE_CONTACT(),
         ])
             ->assertSuccessful()
