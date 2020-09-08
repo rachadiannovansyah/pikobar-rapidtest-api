20adde869cce3203a1a4f0221ef87268b7e136cc
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-08 14:19:39 +0700

    Create HaveInteractedEnum.php

---
+++ app/Enums/HaveInteractedEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self YA()
+ * @method static self TIDAK()
+ * @method static self TIDAK TAHU()
+ */
+
+final class HaveInteractedEnum extends Enum
+{
+}
