5134a55c4bdb49769bda1c8dbb9ce07ad4c3fbb7
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-18 15:50:19 +0700

    Create OccupationTypeListController.php

---
+++ app/Http/Controllers/Master/OccupationTypeListController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+
+use App\Http\Controllers\Controller;
+use App\Entities\OccupationType;
+use App\Http\Resources\OccupationTypeResource;
+
+class OccupationTypeListController extends Controller
+{
+    public function __invoke()
+    {
+        return OccupationTypeResource::collection(OccupationType::all());
+    }
+}
