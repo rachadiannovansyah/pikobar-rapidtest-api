9682c2ae5203748d0f271bb05f523bb515db3831
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-23 11:51:33 +0700

    add routing fasyankes

--- routes/api.php
+++ routes/api.php
@@ -96,5 +96,6 @@ Route::group(['middleware' => 'guest:api'], function () {
         Route::get('areas', 'AreaController@index');
         Route::get('areas/{area}', 'AreaController@show');
         Route::get('occupations', 'OccupationTypeListController');
+        Route::get('fasyankes', 'FasyankesListController');
     });
 });
