665c6e082b46f54aa4bbd732b4be0d3970fd78a1
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-23 17:14:23 +0700

    perbaikan nama column pada property $fillable

--- app/Entities/Fasyankes.php
+++ app/Entities/Fasyankes.php
@@ -9,8 +9,8 @@ class Fasyankes extends Model
     protected $table = 'fasyankes';
 
     protected $fillable = [
-        'nama',
-        'tipe', // Rumah sakit | Dinkes | Puskesmas
-        'kota_id'
+        'name',
+        'type' // Rumah sakit | Dinkes | Puskesmas
+
     ];
 }
