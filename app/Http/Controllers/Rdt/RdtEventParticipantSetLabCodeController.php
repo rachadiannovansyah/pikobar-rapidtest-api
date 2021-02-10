4d782557e6611f3e683b51a6033effba8aa28583
Rachadian Novansyah <32012588+rachadiannovansyah@users.noreply.github.com>
2021-02-10 20:55:04 +0700

    add-checkin-when-update-kode-sample (#122)
    * add-checkin-when-update-kode-sample
    
    * change conditional if from ternary

--- app/Http/Controllers/Rdt/RdtEventParticipantSetLabCodeController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantSetLabCodeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Rdt;
 
 use App\Entities\RdtInvitation;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class RdtEventParticipantSetLabCodeController extends Controller
@@ -21,6 +22,12 @@ class RdtEventParticipantSetLabCodeController extends Controller
          */
         $rdtInvitation                  = RdtInvitation::findOrFail($request->input('rdt_invitation_id'));
         $rdtInvitation->lab_code_sample = $request->input('lab_code_sample');
+        if ($rdtInvitation->attended_at === null) {
+            $rdtInvitation->attended_at = Carbon::now();
+        }
+        if ($rdtInvitation->attend_location === null) {
+            $rdtInvitation->attend_location = $rdtInvitation->event->event_location;
+        }
         $rdtInvitation->save();
 
         return response()->json(['status' => 'OK']);
