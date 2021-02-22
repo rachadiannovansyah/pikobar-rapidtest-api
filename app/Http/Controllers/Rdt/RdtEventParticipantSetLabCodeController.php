1f0949606f79ac896b89b76f0f04321f4533772c
Rachadian Novansyah <32012588+rachadiannovansyah@users.noreply.github.com>
2021-02-22 09:34:27 +0700

    add validation when kode sample already exists or used (#125)
    * add validation when kode sample already exists or used
    
    * phpcs passed
    
    * penyempurnaan syntax code agar lebih mudah dipahami
    
    * fixing miss logic on validation kode sample

--- app/Http/Controllers/Rdt/RdtEventParticipantSetLabCodeController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantSetLabCodeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Rdt;
 
+use App\Entities\RdtEvent;
 use App\Entities\RdtInvitation;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 
 class RdtEventParticipantSetLabCodeController extends Controller
 {
@@ -15,13 +17,28 @@ class RdtEventParticipantSetLabCodeController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(RdtEvent $rdtEvent, Request $request)
     {
         /**
          * @var RdtInvitation $rdtInvitation
          */
-        $rdtInvitation                  = RdtInvitation::findOrFail($request->input('rdt_invitation_id'));
+        $rdtInvitation = RdtInvitation::findOrFail($request->input('rdt_invitation_id'));
+
+        // validasi if kode sample sudah digunakan
+        $inputCodeSample = $request->input('lab_code_sample');
+        $labCodeSampleCount = RdtInvitation::query()
+            ->where('lab_code_sample', $inputCodeSample)
+            ->where('rdt_event_id', $rdtEvent->id)
+            ->exists();
+
+        if ($labCodeSampleCount) {
+            return response()->json([
+                'message' => 'Kode sample sudah ada atau sudah digunakan oleh peserta lain.',
+            ], HttpResponse::HTTP_BAD_REQUEST);
+        }
+
         $rdtInvitation->lab_code_sample = $request->input('lab_code_sample');
+
         if ($rdtInvitation->attended_at === null) {
             $rdtInvitation->attended_at = Carbon::now();
         }
