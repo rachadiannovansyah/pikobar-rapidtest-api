116f30bd71ce2e766c6f3030f035f81cc7c89a11
Novansyah <rachadian.novansyah15@gmail.com>
2021-04-19 13:23:17 +0700

    PSR 12

--- app/Exports/ParticipantListExport.php
+++ app/Exports/ParticipantListExport.php
@@ -5,7 +5,6 @@ namespace App\Exports;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use Maatwebsite\Excel\Concerns\WithColumnWidths;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
@@ -23,20 +22,18 @@ class ParticipantListExport implements
         $this->event = $event;
         $this->number = 1;
     }
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+
     public function collection()
     {
         $data = DB::table('rdt_invitations')
-        ->select(
-            'rdt_applicants.name',
-            'rdt_applicants.birth_date',
-            'rdt_applicants.workplace_name',
-        )
-        ->leftJoin('rdt_applicants', 'rdt_applicants.id', 'rdt_invitations.rdt_applicant_id')
-        ->where('rdt_invitations.rdt_event_id', $this->event->id)
-        ->get();
+            ->select(
+                'rdt_applicants.name',
+                'rdt_applicants.birth_date',
+                'rdt_applicants.workplace_name',
+            )
+            ->leftJoin('rdt_applicants', 'rdt_applicants.id', 'rdt_invitations.rdt_applicant_id')
+            ->where('rdt_invitations.rdt_event_id', $this->event->id)
+            ->get();
 
         return $data;
     }
@@ -52,7 +49,7 @@ class ParticipantListExport implements
         ];
     }
 
-    public function map($event):array
+    public function map($event): array
     {
         return [
             $this->number++,
@@ -66,7 +63,7 @@ class ParticipantListExport implements
     public function registerEvents(): array
     {
         return [
-            AfterSheet::class    => function(AfterSheet $event) {
+            AfterSheet::class => function (AfterSheet $event) {
                 $cellRange = 'A1:W1'; // All headers
                 $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(12);
                 $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setBold(true);
